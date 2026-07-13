adc5a4b91b192e7eefd24e751699b77c86621cf0
Ashar Grosir Parfum <ashargrosirparfumstore@gmail.com>
2026-07-13 18:27:48 +0700

    chore: bump version to 2.0.0, set Indonesia timezone and locale

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'Asia/Jakarta'),
+    'timezone' => 'Asia/Jakarta',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,11 +78,11 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => 'id',
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => 'en',
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => 'id_ID',
 
     /*
     |--------------------------------------------------------------------------
