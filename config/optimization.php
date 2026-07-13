2131438ec2d44b6323a877853a92905a94c6e93e
Ashar Grosir Parfum <ashargrosirparfumstore@gmail.com>
2026-07-13 18:27:46 +0700

    chore: add optimization configuration for route/config/event caching

---
+++ config/optimization.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'route_cache' => env('ROUTE_CACHE_ENABLED', false),
+    'config_cache' => env('CONFIG_CACHE_ENABLED', false),
+    'event_cache' => env('EVENT_CACHE_ENABLED', false),
+];
